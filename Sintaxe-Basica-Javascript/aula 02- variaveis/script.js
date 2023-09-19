91c1c511673b4330a606de473677ab1e5e458028
Laís Yamamoto <lais.yamadev@gmail.com>
2023-09-18 22:40:02 -0300

    aulas de sintaxe basica
    Aulas de Sintaxe básica Javascript

---
+++ Sintaxe-Basica-Javascript/aula 02- variaveis/script.js
@@ -0,0 +1,25 @@
+// tipos primitivos
+
+//boolean
+/*var vouf = false;
+console.log(vouf)
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'lais'
+console.log(typeof(nome));
+
+//como declarar
+var variavel = 'lais';
+variavel = 'yamamoto';
+console.log(variavel);*/
+
+/*let variavel2 = 'lais';
+variavel = 'yamamoto';
+console.log(variavel2);*/
+
+const constante = 'lais';
+console.log(constante);
