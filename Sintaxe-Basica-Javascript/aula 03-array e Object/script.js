1a798f63b219d0bb916af0f5a0bdec46ed1a92c7
Laís Yamamoto <lais.yamadev@gmail.com>
2023-09-20 23:09:08 -0300

    inicio aula 03
    Aula 03 - Vetores e  Objetos

---
+++ Sintaxe-Basica-Javascript/aula 03-array e Object/script.js
@@ -0,0 +1,27 @@
+//O que são vetores, ou arrays
+
+// Como declarar um array
+/*let array = ['string', 1, true];
+console.log(array);*/
+
+//pode guardar vários tipos de dados
+/*let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array);*/
+
+//como acessar um valor dentro do array
+/*let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array[3]);*/
+
+//manipulando arrays
+//forEach - função, por isso os ()
+/*array.forEach(function(item, index){console.log(item, index)});*/
+
+//push - adiciona um novo item no final do array, qualquer tipo de dado
+/*array.push('novo item');
+console.log(array);*/
+
+//pop - remove um item do final do array
+array.pop();
+console.log(array);
+
+
