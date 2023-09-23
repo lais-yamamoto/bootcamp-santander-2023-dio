f59839b9d6703eb60bc2ff421c154bbc33282865
Laís Yamamoto <lais.yamadev@gmail.com>
2023-09-23 13:03:21 -0300

    aula 06 - aprofundando em funçoes
    aprofundando em funções

---
+++ Sintaxe-Basica-Javascript/aula 06-aprofundando em funções/tiposDeFuncao.js
@@ -0,0 +1,31 @@
+// tipos de função
+
+//declarativas
+/*function funcao(){
+    console.log('Sou uma mensagem de uma função declarativa!')
+}
+
+funcao();*/
+
+// expressão de função
+//com nomeação
+/*var funcao = function funcao(){
+    console.log('Sou uma mensagem de função de expressão!')
+}
+
+funcao();*/
+
+//sem nomeação 
+/*ar funcao = function(){
+    console.log('Sou uma mensagem de função de expressão!')
+}
+
+funcao();*/
+
+//arrow function
+/*var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+
+funcao();*/
+
